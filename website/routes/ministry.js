ff940430e53a73501785abbb8c1a0f05b4e3277c
PratikSr99 <pratik.gaz1899@gmail.com>
2020-08-02 13:22:31 +0530

    [website]:add login handler

--- website/routes/ministry.js
+++ website/routes/ministry.js
@@ -30,5 +30,53 @@ router.get('/', ministryAuth, (req, res) => {
     helpers.filter('ministry', req, res)
   })
   
+// login form
+router.get('/login', (req, res) => {
+    res.render('ministry/ministry-login')
+  })
+  
+  
+  
+  
+  // handle login
+  router.post('/login', async (req, res) => {
+    try {
+      const { ministryId, password } = req.body
+      //find user in db
+      const ministry = await Ministry.findOne({ ministryId })
+      //if not found in db
+      if (!ministry) {
+        req.flash('error_message', 'No user found!')
+        return res.redirect('/ministry/login')
+      }
+      //compare password
+      const isMatch = await bcrypt.compare(password, ministry.password)
+      if (!isMatch) {
+        req.flash('error_message', 'Incorrect password!')
+        return res.redirect('/ministry/login')
+      }
+      //create payload
+      const payload = {
+        user: {
+          role: 'ministry',
+          id: ministry.id,
+          ministryId: ministry.ministryId,
+          ministryName: ministry.ministryName
+        }
+      }
+      //generate jwt
+      jwt.sign( payload, process.env.JWT_SECRET, { expiresIn: 12*60*60 }, (err, token) => {
+        if (err) throw err
+        res.cookie('jwt', token, cookieConfig )
+        .redirect('/ministry/dashboard')
+      })
+  
+    }
+    catch (e) {
+      console.log(e)
+      res.redirect('/ministry/login')
+    }
+  })
+  
 
 module.exports = router
